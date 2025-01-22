0f1422c1620b65798b3a3b9dc812608020c4306a
loverboy <loverboykosu@gmail.com>
2025-01-22 21:02:26 +0900

    add sidebar components

--- chartjs/src/App.jsx
+++ chartjs/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import BarChart from './components/BarChart.jsx';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import {Data} from './utils/Data.jsx';
-import Count from './components/Count.jsx'
+import BarChart from './components/BarChart.jsx';
+import Count from './components/Count.jsx';
+import SideMenu from './components/Sidebar.jsx';
+
 function App() {
 
   return (
     <>
+      <SideMenu />
       <Count/>
       <BarChart {...Data} />
     </>
