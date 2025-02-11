827be91107293ba56853216d4267367ac3def3a9
loverboy <loverboykosu@gmail.com>
2025-02-11 22:39:48 +0900

    add prev page button and next page button

--- naruto_app/src/components/Naruto.jsx
+++ naruto_app/src/components/Naruto.jsx
@@ -1,47 +1,90 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "../CSS/naruto.css";
+import { HiH1 } from "react-icons/hi2";
 
 const Naruto = () => {
+  const [page, setPage] = useState(1);
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const limit = 20;
   useEffect(() => {
-    fetchCharacters();
+    fetchCharacters(page);
   }, []);
 
   //fetch characters function
-  const fetchCharacters = async () => {
+  const fetchCharacters = async (page) => {
+    console.log("fetch : ", page);
     const apiUrl = "https://narutodb.xyz/api/character";
-    const result = await axios.get(apiUrl);
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, { params: { page } });
     setCharacters(result.data.characters);
-    console.log(result);
+    console.log(characters.length);
+    setIsLoading(false);
   };
 
+  const previousPage = async () => {
+    const previousPage = page - 1;
+    await fetchCharacters(previousPage);
+    setPage(previousPage);
+  };
+  const nextPage = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  };
   return (
     <>
       <div className="container">
-        <main>
-          <div className="cards-container">
-            {characters.map((character) => {
-              return (
-                <div className="card" key={character.id}>
-                  <img
-                    src={
-                      character.images[0] != null
-                        ? character.images[0]
-                        : "dummy.png"
-                    }
-                    alt="character"
-                    className="card-image"
-                  />
-                  <div className="card-content">
-                    <p>Name : {character.name}</p>
-                    <p>{character.jutsu}</p>
+        {isLoading ? (
+          <div>Now loading ...</div>
+        ) : (
+          <main>
+            <div className="cards-container">
+              {characters.map((character) => {
+                return (
+                  <div className="card" key={character.id}>
+                    <img
+                      src={
+                        character.images[0] != null
+                          ? character.images[0]
+                          : "dummy.png"
+                      }
+                      alt="character"
+                      className="card-image"
+                    />
+                    <div className="card-content">
+                      <p>Name : {character.name}</p>
+                      <p>Jutsu : {character.jutsu}</p>
+                      <p>Debut : {character.debut?.appears ?? "なし"}</p>
+                      <p>
+                        Affiliation :{" "}
+                        {character.personal?.affiliation ?? "なし"}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
-        </main>
+                );
+              })}
+            </div>
+            <div className="pager">
+              <button
+                disabled={page === 1}
+                className="prev"
+                onClick={previousPage}
+              >
+                Previous
+              </button>
+              <span className="page-number">{page}</span>
+              <button
+                disabled={limit > characters.length}
+                className="next"
+                onClick={nextPage}
+              >
+                Next
+              </button>
+            </div>
+          </main>
+        )}
       </div>
     </>
   );
