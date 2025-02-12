61535125f073414298517f5f409a0a1eeee1fbef
loverboy <loverboykosu@gmail.com>
2025-02-12 21:27:50 +0900

    refactor: delete scripts like app.css

--- spotify_app/src/App.jsx
+++ spotify_app/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import "./App.css";
 import Spotify from "./components/Spotify";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Spotify />
