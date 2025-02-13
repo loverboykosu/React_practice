57d6bff05d8952c7fcfe44122d1bdacd83c3bb1e
loverboy <loverboykosu@gmail.com>
2025-02-14 00:37:26 +0900

    feature : add tailwindcss config

--- spotify_app/src/App.jsx
+++ spotify_app/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import Spotify from "./components/Spotify";
-function App() {
+const App = () => {
   return (
-    <>
-      <Spotify />
-    </>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
+        </section>
+      </main>
+    </div>
   );
-}
-
+};
 export default App;
