fcaedd0d73488a00738988dc5990a6eff393a43d
loverboy <loverboykosu@gmail.com>
2025-02-26 20:09:36 +0900

    feat : poke app add axios func to get poke name

--- poke_app/src/components/Poke.jsx
+++ poke_app/src/components/Poke.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 const Poke = () => {
+  const [pokeList, setPokeList] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
+  useEffect(() => {
+    fetchPoke();
+  }, []);
+  const fetchPoke = async () => {
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+    const result = await axios.get(apiUrl);
+    const pokeLists = result.data.results.map((item) => {
+      // console.log(item);
+      return item.name;
+    });
+    setPokeList(pokeLists);
+    setIsLoad(true);
+  };
   return (
     <>
-      <div className="bg-sky-500">
-        <h1 className="text-white">hello</h1>
+      <div className="bg-black">
+        <h1 className="text-white text-center p-5 text-2xl">Poke app</h1>
       </div>
+      <h2>{isLoad ? pokeList : ""}</h2>
     </>
   );
 };
